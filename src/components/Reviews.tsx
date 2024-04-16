f9f0b7e03ff66ad60546fcbebfab06fd4e83c5cc
unknown <mehetajkhandaker@gmail.com>
2024-04-16 22:22:38 +0600

    get started compo errors sovled

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,9 +1,9 @@
-
+import quote_mark from './assets/hero_banner.png'
 
 const Reviews = () => {
     return (
-        <div>
-            
+        <div className='reviews'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, optio, eligendi rem nemo non earum a architecto quaerat quo exercitationem soluta ipsa assumenda? In debitis, dolores voluptatem aperiam nam autem.
         </div>
     );
 };
